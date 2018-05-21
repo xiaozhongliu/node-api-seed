4b854f5bfcf6519f2e83df2adf1a210b051320f2
xiaozhongliu <xiaozhong.liu@outlook.com>
2018-05-21 20:26:06 +0800

    test committing with the correct account name

--- message.js
+++ message.js
@@ -4,7 +4,7 @@ module.exports = {
     AuthFail: { code: 10001, msg: `身份验证失败:${config.API_NAME}` },
     NotFound: { code: 10002, msg: '请求的资源不存在:@param' },
 
-    CommonErr: { code: 11000 }, // 无需特殊处理的自定义错误都用这个代码
+    CommonErr: { code: 11000 },
     SysTypeEmpty: { code: 11001 },
     UsernameEmpty: { code: 11002 },
     PasswordEmpty: { code: 11003 },
